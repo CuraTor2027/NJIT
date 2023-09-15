a8c57cb71ba2e42f03bc02a1d9cc6d27dba7a446
CuraTor2027 <143978411+CuraTor2027@users.noreply.github.com>
2023-09-15 14:40:53 -0400

    Add files via upload

---
+++ CS 100-019/HW/HW1_RyanArokiaRaj.py
@@ -0,0 +1,47 @@
+'''
+    Ryan Arokia-Raj
+    20230909
+    CS100-019
+    HW1_RyanArokiaRaj.py
+'''
+
+# Exercise 5b
+age = 21
+
+# Exercise 5c
+height = 5.7
+
+# Exercise 5d
+name = "Ryan Arokia-Raj"
+
+'''
+1.1
+    1. Syntax Error when leaving out a parentheses
+    2. Syntax Error when leaving out a quotation
+    3. 2++2 prints out 4
+    4. Syntax Error, leading zeros not allowed
+    5. depending on the value types, they get concatenated
+
+1.2
+    1. 2562 seconds in 42 min 42 sec
+    2. 6.2 miles in 10 km
+    3. 6 min 52 sec per mile, 8.63 mph
+'''
+
+x = y = 1
+print(x);
+print(y)
+'''
+2.1
+    1. Syntax Error when doing 42 = n
+    2. no error, x and y are assigned 1
+    3. no error when placing ; behind statement
+    4. Syntax Error when placing . behind a statement
+    5. Name Error when doing 'xy'
+
+2.2
+    1. V = 523.58
+    2. $945.45
+    3. 7:21
+
+'''
